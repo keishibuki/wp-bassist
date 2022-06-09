034f3fae0f9416e87927258b79afd5e331f9c4cf
keishibuki <kei.play52@gmail.com>
2022-06-09 12:05:48 +0900

    投稿リストを追加

---
+++ inc/loop/card-post.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Loop card component for post.
+ *
+ * @package WordPress
+ */
+
+?>
+
+<li <?php post_class(); ?>>
+	<picture class="block">
+		<a href="<?php the_permalink(); ?>">
+			<?php
+				$thumbnail_url = get_the_post_thumbnail_url( '', 'medium' );
+			if ( $thumbnail_url ) :
+				?>
+			<img class="aspect-3/2 object-cover w-full" src="<?php echo esc_attr( $thumbnail_url ); ?>"
+				alt="<?php the_title(); ?>">
+			<?php else : ?>
+			<img class="aspect-3/2 object-cover w-full" src="//placehold.jp/cccccc/999999/280x200.png?text=NoImage"
+				alt="<?php the_title(); ?>">
+			<?php endif; ?>
+		</a>
+	</picture>
+	<time datetime="<?php the_time( 'Y-m-d' ); ?>"
+		class="block text-sm leading-none mt-3"><?php the_time( 'Y-m-d' ); ?></time>
+	<div class="flex items-center gap-2 my-3">
+		<?php $categories = get_the_terms( $post->ID, 'category' ); ?>
+		<?php if ( $categories ) : ?>
+			<?php foreach ( $categories as $post_cat ) : ?>
+			<a href="<?php echo esc_url( get_term_link( $post_cat->term_id, 'category' ) ); ?>"
+				class="inline-block px-3 py-1 text-sm text-white bg-primary">
+				<?php echo esc_html( $post_cat->name ); ?>
+			</a>
+			<?php endforeach; ?>
+		<?php endif; ?>
+	</div>
+	<h3 class="font-bold line-clamp-2">
+		<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+	</h3>
+</li>
--- index.php
+++ index.php
@@ -5,13 +5,23 @@
  * @package WordPress
  */
 
-get_header();
-if ( have_posts() ) :
-	while ( have_posts() ) :
-		the_post();
-		get_template_part( 'entry' );
-		comments_template();
-endwhile;
-endif;
-get_template_part( 'nav', 'below' );
+?>
+<?php get_header(); ?>
+<div class="px-4 md:px-3 md:mx-auto md:container md:max-w-5xl">
+	<?php if ( have_posts() ) : ?>
+	<ul class="grid grid-cols-2 gap-x-4 gap-8 md:gap-x-8 md:grid-cols-3">
+		<?php
+		while ( have_posts() ) {
+			the_post();
+			get_template_part( 'inc/loop/card', 'post' );
+		}
+		?>
+	</ul>
+	<?php endif; ?>
+	<?php
+	get_template_part( 'nav', 'below' );
+	?>
+</div>
+<?php
 get_footer();
+?>
