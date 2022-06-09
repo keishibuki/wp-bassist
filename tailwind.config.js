520eaa113a548f5201462743305ccbee5b779e1b
keishibuki <kei.play52@gmail.com>
2022-06-09 12:00:32 +0900

    ・TailwindでWordPress/theme.jsonの色設定が使えるように ・Tailwindでline-clampが使えるように ・Tailwindのaspect-ratioの設定に3/2を追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,32 @@
+const fs = require("fs");
+
+const themeJson = fs.readFileSync("./theme.json");
+const theme = JSON.parse(themeJson);
+
+const colors = theme.settings.color.palette.reduce((acc, item) => {
+  const [color, number] = item.slug.split("-");
+  if (undefined !== number) {
+    if (!acc[color]) {
+      acc[color] = {};
+    }
+    acc[color][number] = item.color;
+  } else {
+    acc[color] = item.color;
+  }
+
+  return acc;
+}, {});
+
 module.exports = {
   content: ["./**/*.php"],
   theme: {
-    extend: {},
+    extend: {
+      colors,
+      aspectRatio: {
+        "4/3": "4 / 3",
+        "3/2": "3 / 2",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/line-clamp")],
 };
